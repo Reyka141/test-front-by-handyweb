fbd7168bb95ba87ee5f1b802401af211726a7eca
Andrii <youdronveduta@gmail.com>
2024-10-28 01:53:21 +0100

    add src/components/Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="font-[family-name:var(--font-satoshi)] font-normal h-[51px] bg-[var(--footer)] opacity-90 ">
+      <div className="max-width">
+        <p className="pt-4 text-white text-[10px]/5">© ООО “Gushop” 2002—2019. All rights reserved</p>
+      </div>
+    </footer>
+  );
+}
